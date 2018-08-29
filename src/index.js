ad97f37c80408f3861fa6751ca273717d607d0a4
fannyhasbi <fnyhsbi@gmail.com>
2018-08-29 14:04:37 +0700

    Add session

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+function generateSession(){
+  var res = "";
+  var str = "abcdefghijklmnopqrstuvwxyz1234567890";
+  for(let i = 0; i < 15; i++){
+    res += str.charAt(Math.floor(Math.random() * str.length));
+  }
+  return res;
+}
+
+if(sessionStorage.getItem('USERSESSID') === null){
+  sessionStorage.setItem('USERSESSID', generateSession());
+}
+
+console.log(sessionStorage.getItem('USERSESSID'));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
